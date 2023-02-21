faee57062d79ce7b70cb28765afa55c1c1ef1a7e
Zaiyd Mala <zaiydhmala@gmail.com>
2023-02-21 16:35:39 -0500

    Connect firebase and design login page

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
 export default function Nav() {
     return(
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <nav className="flex justify-between items-center py-10">
+            <Link href='/'>
+                <button className="text-lg font-medium">Shower Thoughts</button>
+            </Link>
+            <ul className="flex items-center gap-10">
+                <Link href={"/auth/login"} className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8'>
+                    Join Now!
+                </Link>
+            </ul>
+        </nav>
     );
 }
