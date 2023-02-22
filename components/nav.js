f4132a233a7ffb42e1fbad376dc81170d8800e34
Zaiyd Mala <zaiydhmala@gmail.com>
2023-02-21 22:52:03 -0500

    Add user dashboard

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,31 @@
 import Link from "next/link";
+import { auth } from '../utils/firebase';
+import { useAuthState } from "react-firebase-hooks/auth";
+
 export default function Nav() {
+    const [user, loading] = useAuthState(auth);
+    console.log(user);
     return(
         <nav className="flex justify-between items-center py-10">
             <Link href='/'>
                 <button className="text-lg font-medium">Shower Thoughts</button>
             </Link>
             <ul className="flex items-center gap-10">
-                <Link href={"/auth/login"} className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8'>
+                {!user && (
+                    <Link href={"/auth/login"} className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8'>
                     Join Now!
-                </Link>
+                    </Link>
+                )}
+                {user && (
+                    <div className="flex items-center gap-6">
+                        <Link href='/post'>
+                            <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-md text-sm">Post</button>
+                        </Link>
+                        <Link href='/dashboard'>
+                            <img className="w-12 rounded-full cursor-pointer" src={user.photoURL}  alt="user-dp"/>
+                        </Link>
+                    </div>
+                )}
             </ul>
         </nav>
     );
