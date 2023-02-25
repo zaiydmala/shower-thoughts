167c3022dc528c857c50f3025f46ff9c101ba52f
Zaiyd Mala <zaiydhmala@gmail.com>
2023-02-24 19:04:57 -0500

    Comments are up and running but with a bug :(

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,7 +2,8 @@
  import { useRouter } from "next/router";
  import { useEffect, useState } from "react";
  import {auth, db } from '../utils/firebase';
- import { Toast } from "react-toastify";
+ import { toast, Toast } from "react-toastify";
+import { arrayUnion, doc, Timestamp, updateDoc } from "firebase/firestore";
 
 
  export default function Details() {
@@ -10,6 +11,40 @@
     const routeData = router.query;
     const [message, setMessage] = useState('');
     const [allMessages, setAllMessages] = useState([]);
+    
+
+    //Submit a message
+    const submitMessage = async() => {
+        //Check if user is logged in
+        if(!auth.currentUser) return router.push('/auth/login');
+        
+        if(!message) {
+            toast.error('Dont leave and empty message', {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 1500,
+            });
+            return; 
+        }
+        if(message.length > 50) {
+            toast.error('Nobody got time for your longass opinions, got it? ', {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 1500,
+            });
+            return;
+        }
+
+        const docRef = doc(db,'posts', routeData.id);
+        await updateDoc(docRef, {
+            comments: arrayUnion({
+                message,
+                avatar: auth.currentUser.photoURL,
+                userName: auth.currentUser.displayName,
+                time: Timestamp.now(),
+            })
+        })
+
+        setMessage('');
+    };
     return(
         <div>
             <Message {...routeData}></Message>
@@ -20,7 +55,7 @@
                         placeholder='send a message' 
                         className="bg-gray-800 w-full p-2 text-white text-sm "
                     />
-                    <button className="bg-cyan-500 text-white py-2 px-4 text-sm">Submit</button>
+                    <button onClick={submitMessage} className="bg-cyan-500 text-white py-2 px-4 text-sm">Submit</button>
                 </div>
                 <div className="py-6">
                     <h2 className="font-bold">Comments</h2>
